f987b5487a2e30fdacb5ab01f02d2ae4d5d2f399
MariosPaschos <mariospaschos@gmail.com>
2019-05-29 22:30:18 +0100

    Added boolean functions to validate whether a given string contains a valid number, a valid double or a valid integer

--- Calc.hs
+++ Calc.hs
@@ -20,6 +20,7 @@ op '*' = (*)
 op '/' = (/)
 
 
+
 calculator = do
   x <- prompt "calc>"
   putStrLn x
@@ -35,13 +36,29 @@ tokenize expr
       left = trimSpaces $ takeWhile (/='+') expr
       right = trimSpaces $ tail $ dropWhile (/='+') expr
       action = head $ trimSpaces $ dropWhile (/='+') expr
-      result = (read left :: Integer) + (read right :: Integer)
+      result = op (action) 4 3--(read left :: Integer) (read right :: Integer)
 
 
 isValidNumber val
-  | all isNumber val == True = True
+  | any (== True) [isValidInt val, isValidDouble val] = True
   | otherwise = False
 
+isValidInt val
+  | (all isDigit val) && (not $ hasDot val) = True
+  | otherwise = False
+
+isValidDouble val
+  | (hasDot val) && (all isDigit $ filter (/='.') val) = True
+  | otherwise = False              
+
+hasDot val
+  | (length $ filter (=='.') val) == 1 = True
+  | otherwise = False
+  
+
+
+-- parseNumber str = 
+--   | read "str" :: Integer 
 
 trimSpaces :: String -> String
 trimSpaces expr = filter (not . isSpace) expr
